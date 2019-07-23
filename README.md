64c0b743590773398f3b27f3eebb1d0b6af54d7d
075shikhovav <shikhov@gmail.com>
2019-07-23 14:20:34 +0500

    add files

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# tg-channel-watcher
-Отслеживание сообщений в каналах Telegram
+# Описание
+Приложение отслеживает новые сообщения в каналах Telegram и пересылает те, которые подпадают под ключевые слова. Пример работы: https://t.me/travelekb
+
+# Реализация
+Написано на Python 3.7 с использованием фреймворка Telethon. Хранение данных в БД Cloudant (облачный сервис IBM Cloud). Для развертывания прилагается Dockerfile и плейбук Ansible.
+
+# Развертывание
+- Создать в IBM Cloud ресурс Cloudant. Создать Service credentials, открыть View credentials, сохранить как `app/src/creds.json`
+- Добавить в `app/src/creds.json` ключи `dbname` и `docname` — имя БД и имя документа, в котором будут перечислены каналы. Пример:
+ ```
+ "dbname": "tgcw",
+ "docname": "input_channels",
+```
+- Создать БД с именем `tgcw`, в ней создать документы `input_channels` и `settings`
+- В документе `input_channels` перечислить имена Telegram-каналов и номер последнего сообщения, с которого начинать отслеживание. Чтобы начать с последнего, указать 0. Пример:
+```
+"channels": {
+    "piratesru": 0,
+    "vandroukiru": 0,
+    "turs_sale": 0
+  },
+```
+- Заполнить документ `settings`:
+    - keywords — ключевые слова (могут быть регулярными выражениями)
+    - sleeptimer — период опроса каналов в секундах
+    - output_channel — имя целевого канала, куда будут пересылаться сообщения
+    - api_id, api_hash —  см.: https://my.telegram.org
+    - session — строка сеанса, полученная после аутентификации через фреймворк Telethon. См.: https://telethon.readthedocs.io/en/latest/concepts/sessions.html#string-sessions
+
+Пример:
+```
+"keywords": [
+    "екатеринбург",
+    "ебург",
+    "екб"
+  ],
+  "sleeptimer": 300,
+  "output_channel": "travelekb",
+  "api_id": 123456,
+  "api_hash": "your_api_hash",
+  "session": "your_string_session"
+```
+- Установить Docker вручную или через плейбук `ansible/aws2_install_docker.yml` (написан для образа Amazon Linux 2 AMI)
+- Развернуть Dockerfile вручную или через плейбук `ansible/rebuild_container.yml`
---
+++ app/src/app.py
@@ -0,0 +1,70 @@
+import json
+import re
+import time
+from datetime import datetime
+from datetime import timedelta
+
+from cloudant.client import Cloudant
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+from telethon.tl.functions.contacts import ResolveUsernameRequest
+from telethon.tl.functions.messages import ForwardMessagesRequest
+
+creds = json.load(open('creds.json'))
+
+dbname = creds['dbname']
+docname = creds['docname']
+dbuser = creds['username']
+dbpass = creds['password']
+dburl = 'https://' + creds['host']
+dbclient = Cloudant(dbuser, dbpass, url=dburl, connect=True)
+
+db = dbclient[dbname]
+settings = db['settings']
+api_id = settings['api_id']
+api_hash = settings['api_hash']
+session = settings['session']
+output_channel = settings['output_channel']
+
+tg = TelegramClient(StringSession(session), api_id, api_hash)
+tg.start()
+
+doc = db[docname]
+channels = doc['channels']
+
+peers = {}
+for channel in channels:
+    peers[channel] = tg(ResolveUsernameRequest(channel))
+
+output_peer = tg(ResolveUsernameRequest(output_channel))
+
+dbclient.disconnect()
+
+while True:
+    dbclient.connect()
+    db = dbclient[dbname]
+    settings = db['settings']
+    keywords = settings['keywords']
+    sleeptimer = settings['sleeptimer']
+    doc = db[docname]
+    channels = doc['channels']
+
+    for channel in channels:
+        saved_msg_id = channels[channel]
+        last_msg_id = tg.get_messages(channel, limit=1)[0].id
+        if saved_msg_id != 0:
+            if last_msg_id > saved_msg_id:
+                new_msg_count = last_msg_id - saved_msg_id
+                for msg in reversed(tg.get_messages(channel, limit=new_msg_count)):
+                    if msg.message and msg.id > saved_msg_id:
+                        for keyword in keywords:
+                            if re.search(keyword, msg.message.lower()):
+                                tg(ForwardMessagesRequest(from_peer=peers[channel], id=[msg.id], to_peer=output_peer))
+                                break
+        channels[channel] = last_msg_id
+
+    doc['lastupdate'] = str(datetime.now()+timedelta(hours=5))
+    doc.save()
+    dbclient.disconnect()
+
+    time.sleep(sleeptimer)

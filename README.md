4bb83ef6bf709204e4dc8b4b954210db3c421e59
Aleksey Shikhov @ home <shikhov@gmail.com>
2021-07-03 21:19:07 +0500

    v2.0: multiple profiles

--- README.md
+++ README.md
@@ -6,40 +6,51 @@
 
 # Развертывание
 - Создать в IBM Cloud ресурс Cloudant. Создать Service credentials, открыть View credentials, сохранить как `app/src/creds.json`
-- Добавить в `app/src/creds.json` ключи `dbname` и `docname` — имя БД и имя документа, в котором будут перечислены каналы. Пример:
+- Добавить в `app/src/creds.json` ключ `dbname` — имя БД в сервисе Cloudant, в которой будут храниться настройки. Пример:
  ```
- "dbname": "tgcw",
- "docname": "input_channels",
+ "dbname": "tg-сhannel-watcher",
 ```
-- Создать БД с именем `tgcw`, в ней создать документы `input_channels` и `settings`
-- В документе `input_channels` перечислить имена Telegram-каналов и номер последнего сообщения, с которого начинать отслеживание. Чтобы начать с последнего, указать 0. Пример:
-```
-"channels": {
-    "piratesru": 0,
-    "vandroukiru": 0,
-    "turs_sale": 0
-  },
-```
+- Создать БД с именем `tg-сhannel-watcher`, в ней создать документ `settings`
+
 - Получить api_id и api_hash для приложения на https://my.telegram.org
 - С помощью фреймворка Telethon произвести аутентификацию на сервере Telegram, сохранить string session. См.: https://telethon.readthedocs.io/en/latest/concepts/sessions.html#string-sessions
 - Заполнить документ `settings`:
-    - `keywords` — ключевые слова (могут быть регулярными выражениями)
+    - `profiles` — список профилей с настройками каналов (имя профиля = имя документа в БД)
     - `sleeptimer` — период опроса каналов в секундах
     - `output_channel` — имя целевого канала, куда будут пересылаться сообщения
     - `api_id`, `api_hash`, `session` —  значения, полученные на предыдущих шагах
 
 Пример:
 ```
-"keywords": [
+"profiles": [
+    "travelekb"
+  ],
+  "sleeptimer": 300,
+  "api_id": 123456,
+  "api_hash": "your_api_hash",
+  "session": "your_string_session"
+```
+
+- Создать и заполнить документы профилей:
+    - `channels` — список имен Telegram-каналов и номер последнего сообщения, с которого начинать отслеживание. Чтобы начать с последнего, указать 0
+    - `keywords` — ключевые слова (могут быть регулярными выражениями)
+    - `any_matching` — true, если достаточно совпадения одного из ключевых слов; false, если совпадения должны быть по всем
+    - `output_channel` — имя целевого канала, куда будут пересылаться сообщения
+
+Пример профиля travelekb:
+```
+"channels": {
+    "piratesru": 0,
+    "vandroukiru": 0,
+    "turs_sale": 0
+  },
+  "keywords": [
     "екатеринбург",
     "ебург",
     "екб"
   ],
-  "sleeptimer": 300,
+  "any_matching": true,
   "output_channel": "travelekb",
-  "api_id": 123456,
-  "api_hash": "your_api_hash",
-  "session": "your_string_session"
 ```
 - Установить Docker вручную или через плейбук `ansible/aws2_install_docker.yml` (написан для образа Amazon Linux 2 AMI)
 - Развернуть Dockerfile вручную или через плейбук `ansible/rebuild_container.yml`
--- app/src/app.py
+++ app/src/app.py
@@ -7,13 +7,10 @@ from datetime import timedelta
 from cloudant.client import Cloudant
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
-from telethon.tl.functions.contacts import ResolveUsernameRequest
-from telethon.tl.functions.messages import ForwardMessagesRequest
 
 creds = json.load(open('creds.json'))
 
 dbname = creds['dbname']
-docname = creds['docname']
 dbuser = creds['username']
 dbpass = creds['password']
 dburl = 'https://' + creds['host']
@@ -24,48 +21,48 @@ settings = db['settings']
 api_id = settings['api_id']
 api_hash = settings['api_hash']
 session = settings['session']
-output_channel = settings['output_channel']
 
 tg = TelegramClient(StringSession(session), api_id, api_hash)
 tg.start()
-
-doc = db[docname]
-channels = doc['channels']
-
-peers = {}
-for channel in channels:
-    peers[channel] = tg(ResolveUsernameRequest(channel))
-
-output_peer = tg(ResolveUsernameRequest(output_channel))
-
-dbclient.disconnect()
 
 while True:
     dbclient.connect()
     db = dbclient[dbname]
     settings = db['settings']
-    keywords = settings['keywords']
+    profiles = settings['profiles']
     sleeptimer = settings['sleeptimer']
-    doc = db[docname]
-    channels = doc['channels']
 
-    for channel in channels:
-        saved_msg_id = channels[channel]
-        last_msg_id = tg.get_messages(channel, limit=1)[0].id
-        channels[channel] = last_msg_id
-        if saved_msg_id == 0: continue
-        if last_msg_id <= saved_msg_id: continue
-        new_msg_count = last_msg_id - saved_msg_id
-        for msg in reversed(tg.get_messages(channel, limit=new_msg_count)):
-            if not msg.message: continue
-            if msg.id <= saved_msg_id: continue
-            for keyword in keywords:
-                if re.search(keyword, msg.message.lower()):
-                    tg(ForwardMessagesRequest(from_peer=peers[channel], id=[msg.id], to_peer=output_peer))
-                    break
+    for profile_name in profiles:
+        profile = db[profile_name]
+        channels = profile['channels']
+        keywords = profile['keywords']
+        output_channel = profile['output_channel']
+        any_matching = profile['any_matching']
+        for channel in channels:
+            saved_msg_id = channels[channel]
+            last_msg_id = tg.get_messages(channel, limit=1)[0].id
+            channels[channel] = last_msg_id
+            if saved_msg_id == 0: continue
+            if last_msg_id <= saved_msg_id: continue
+            new_msg_count = last_msg_id - saved_msg_id
+            for msg in tg.get_messages(channel, limit=new_msg_count, reverse=True):
+                if not msg.message: continue
+                if msg.id <= saved_msg_id: continue
+                match = False
+                matched_count = 0
+                for keyword in keywords:
+                    if re.search(keyword, msg.message.lower()):
+                        matched_count += 1
+                        if any_matching:
+                            match = True
+                            break
+                    elif not any_matching: break
 
-    doc['lastupdate'] = str(datetime.now()+timedelta(hours=5))
-    doc.save()
+                if match or matched_count == len(keywords):
+                    msg.forward_to(output_channel)
+
+        profile['lastupdate'] = str(datetime.now()+timedelta(hours=5))
+        profile.save()
+
     dbclient.disconnect()
-
     time.sleep(sleeptimer)

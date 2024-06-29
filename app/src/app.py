9a875463222c951e275a535474b2436e7cbd22e7
Aleksey Shikhov <shikhov@gmail.com>
2024-06-29 12:36:01 +0500

    move functions into class

--- app/src/app.py
+++ app/src/app.py
@@ -12,16 +12,16 @@ from config import CONNSTRING, DBNAME
 
 class Profile:
     def __init__(self, doc) -> None:
+        self.doc = doc
         self.name = doc['name']
         self.channels = doc['channels']
         self.output = doc['output']
+        self.count = 0
 
     def process(self):
-        # channels = self.doc['channels']
-        # output = profile_doc['output']
         for channel in self.channels:
             time.sleep(DELAY)
-            i += 1
+            self.count += 1
             logging.info(f'[{self.name}]{channel}')
             try:
                 last_msg_id = tg.get_messages(channel, limit=1)[0].id
@@ -29,148 +29,155 @@ class Profile:
                 logging.error('Error receiving messages!')
                 continue
             saved_msg_id = self.channels[channel]
-            channels[channel] = last_msg_id
-            if saved_msg_id == 0: continue
+            self.doc['channels'][channel] = last_msg_id
             if last_msg_id <= saved_msg_id: continue
-            messages = getMessages()
-            for entry in output:
-                output_channel = entry['output_channel']
-                rules = entry.get('rules')
-                any_matching = entry.get('any_matching')
-                hide_forward = entry.get('hide_forward')
-                all_messages = entry.get('all_messages')
-                ex_rules = entry.get('ex_rules')
+            if saved_msg_id == 0: continue
+            messages = self.getMessages(channel, saved_msg_id, last_msg_id)
+            if not messages: continue
+            for output in self.output:
+                action = Action(output, messages, channel)
+                action.run()
 
-                if all_messages:
-                    processAllMessages()
+        self.doc['lastupdate'] = str(datetime.now()+timedelta(hours=5))
+        db.profiles.update_one({'name' : self.name}, {'$set': self.doc})
+
+
+    def getMessages(self, channel, saved_msg_id, last_msg_id):
+        albums = {}
+        non_album = []
+
+        for msg in reversed(tg.get_messages(channel, limit=last_msg_id-saved_msg_id)):
+            if msg.id <= saved_msg_id: continue
+            if msg.grouped_id:
+                if msg.grouped_id in albums:
+                    albums[msg.grouped_id].append(msg)
                 else:
-                    processRules()
-
-def processRules():
-    for msg_id in sorted(messages):
-        msg = messages[msg_id][0]
-        if checkRules(msg):
-            forwardMessage(msg)
-
-
-def processAllMessages():
-    for msg_id in sorted(messages):
-        if checkExRules(messages[msg_id][0]):
-            try:
-                tg.forward_messages(output_channel, messages[msg_id])
-            except Exception:
-                pass
-
-
-def checkExRules(msg):
-    if not msg.message: return True
-    if not ex_rules: return True
-    for ex_rule in ex_rules:
-        if re.search(ex_rule['regex'], msg.message, re.IGNORECASE | re.DOTALL):
-            return False
-    return True
-
-
-def getMessages():
-    albums = {}
-    non_album = []
-
-    new_msg_count = last_msg_id - saved_msg_id
-    for msg in reversed(tg.get_messages(channel, limit=new_msg_count)):
-        if msg.id <= saved_msg_id: continue
-        if msg.grouped_id:
-            if msg.grouped_id in albums:
-                albums[msg.grouped_id].append(msg)
+                    albums[msg.grouped_id] = [msg]
             else:
-                albums[msg.grouped_id] = [msg]
+                non_album.append(msg)
+
+        msg_array = {}
+        for album in albums.values():
+            msg_array[album[0].id] = album
+        for msg in non_album:
+            msg_array[msg.id] = [msg]
+
+        return dict(sorted(msg_array.items()))
+
+
+class Action:
+    def __init__(self, output, messages, channel) -> None:
+        self.output_channel = output['output_channel']
+        self.rules = output.get('rules')
+        self.any_matching = output.get('any_matching')
+        self.hide_forward = output.get('hide_forward')
+        self.all_messages = output.get('all_messages')
+        self.ex_rules = output.get('ex_rules')
+        self.messages = messages
+        self.channel = channel
+
+    def run(self):
+        if self.all_messages:
+            self.processAllMessages()
         else:
-            non_album.append(msg)
+            self.processRules()
 
-    msg_array = {}
-    for album in albums.values():
-        msg_array[album[0].id] = album
-    for msg in non_album:
-        msg_array[msg.id] = [msg]
-
-    return msg_array
-
-
-def checkRules(msg):
-    if not msg.message: return False
-
-    matched_count = 0
-    for rule in rules:
-        rule_regex = rule['regex']
-        rule_eval = rule.get('eval')
-        rule_result = False
-        if rule_eval:
-            for m in re.finditer(rule_regex, msg.message, re.IGNORECASE | re.DOTALL):
+    def processAllMessages(self):
+        for msg in self.messages.values():
+            if self.checkExRules(msg[0]):
                 try:
-                    rule_result = eval(rule_eval)
+                    tg.forward_messages(self.output_channel, msg)
                 except Exception:
-                    logging.info('Error with eval: ' + rule_eval)
-                    break
+                    pass
 
-                if rule_result:
-                    matched_count += 1
-                    break
-        else:
-            if re.search(rule_regex, msg.message, re.IGNORECASE | re.DOTALL):
-                rule_result = True
-                matched_count += 1
-
-        if rule_result and any_matching:
-            return True
-        if not rule_result and not any_matching:
-            return False
-
-    if matched_count == len(rules):
+    def checkExRules(self, msg):
+        if not msg.message: return True
+        if not self.ex_rules: return True
+        for ex_rule in self.ex_rules:
+            if re.search(ex_rule['regex'], msg.message, re.IGNORECASE | re.DOTALL):
+                return False
         return True
 
-    return False
+    def processRules(self):
+        for msg in self.messages.values():
+            if self.checkRules(msg[0]):
+                self.forwardMessage(msg[0])
+
+    def checkRules(self, msg):
+        if not msg.message: return False
+        matched_count = 0
+        for rule in self.rules:
+            rule_regex = rule['regex']
+            rule_eval = rule.get('eval')
+            rule_result = False
+            if rule_eval:
+                for m in re.finditer(rule_regex, msg.message, re.IGNORECASE | re.DOTALL):
+                    try:
+                        rule_result = eval(rule_eval)
+                    except Exception:
+                        logging.info('Error with eval: ' + rule_eval)
+                        break
+
+                    if rule_result:
+                        matched_count += 1
+                        break
+            else:
+                if re.search(rule_regex, msg.message, re.IGNORECASE | re.DOTALL):
+                    rule_result = True
+                    matched_count += 1
+
+            if rule_result and self.any_matching:
+                return True
+            if not rule_result and not self.any_matching:
+                return False
+
+        if matched_count == len(self.rules):
+            return True
+
+        return False
 
 
-def forwardMessage(msg):
-    if not msg.from_id:
-        # message from channel
-        foo = f'{output_channel}_{msg.message}'.encode('utf-8')
-        msg_hash = md5(foo).hexdigest()
-        if msg_hash in sent: return
-        if hide_forward:
-            trimMessage(msg, False)
-            tg.send_message(output_channel, msg)
+    def forwardMessage(self, msg):
+        if not msg.from_id:
+            # message from channel
+            foo = f'{self.output_channel}_{msg.message}'.encode('utf-8')
+            msg_hash = md5(foo).hexdigest()
+            if msg_hash in sent: return
+            if self.hide_forward:
+                self.trimMessage(msg, False)
+                tg.send_message(self.output_channel, msg)
+            else:
+                msg.forward_to(self.output_channel)
         else:
-            msg.forward_to(output_channel)
-    else:
-        # message from chat
-        foo = f'{msg.from_id.user_id}_{msg.message}'.encode('utf-8')
-        msg_hash = md5(foo).hexdigest()
-        if msg_hash in sent: return
-        trimMessage(msg, True)
-        tg.send_message(output_channel, msg)
+            # message from chat
+            foo = f'{msg.from_id.user_id}_{msg.message}'.encode('utf-8')
+            msg_hash = md5(foo).hexdigest()
+            if msg_hash in sent: return
+            self.trimMessage(msg, True)
+            tg.send_message(self.output_channel, msg)
 
-    sent[msg_hash] = 1
+        sent[msg_hash] = 1
 
+    def trimMessage(self, msg, always_include_link):
+        max_length = 4096
+        if msg.photo or msg.video or msg.audio or msg.document:
+            max_length = 1024
 
-def trimMessage(msg, always_include_link):
-    max_length = 4096
-    if msg.photo or msg.video or msg.audio or msg.document:
-        max_length = 1024
+        if 'joinchat' in self.channel:
+            channel_id = str(msg.chat_id).replace('-100', '')
+            link = f'\nt.me/c/{channel_id}/{msg.id}\n{self.channel}'
+        else:
+            link = f'\nt.me/{self.channel}/{msg.id}'
+        trim_length = max_length - len(link) - 1
 
-    if 'joinchat' in channel:
-        channel_id = str(msg.chat_id).replace('-100', '')
-        link = f'\nt.me/c/{channel_id}/{msg.id}\n{channel}'
-    else:
-        link = f'\nt.me/{channel}/{msg.id}'
-    trim_length = max_length - len(link) - 1
-
-    if always_include_link:
-        if len(msg.raw_text) + len(link) > max_length:
+        if always_include_link:
+            if len(msg.raw_text) + len(link) > max_length:
+                msg.raw_text = msg.raw_text[0:trim_length] + '…'
+            msg.raw_text += link
+        elif len(msg.raw_text) > max_length:
             msg.raw_text = msg.raw_text[0:trim_length] + '…'
-        msg.raw_text += link
-    elif len(msg.raw_text) > max_length:
-        msg.raw_text = msg.raw_text[0:trim_length] + '…'
-        msg.raw_text += link
+            msg.raw_text += link
+
 
 DELAY = 10
 
@@ -191,7 +198,7 @@ while True:
     settings = db.settings.find_one({'_id': 'settings'})
     profiles = settings['profiles']
     sleeptimer = settings['sleeptimer']
-    i = 0
+    count = 0
 
     for profile in profiles:
         if not profile['enable']: continue
@@ -201,39 +208,11 @@ while True:
             logging.warning(f'Profile doc "{profile_name}" not found!')
             continue
 
-        # channels = profile_doc['channels']
-        output = profile_doc['output']
-        for channel in channels:
-            time.sleep(DELAY)
-            i += 1
-            logging.info(f'[{profile_name}]{channel}')
-            try:
-                last_msg_id = tg.get_messages(channel, limit=1)[0].id
-            except Exception:
-                logging.error('Error receiving messages!')
-                continue
-            saved_msg_id = channels[channel]
-            channels[channel] = last_msg_id
-            if saved_msg_id == 0: continue
-            if last_msg_id <= saved_msg_id: continue
-            messages = getMessages()
-            for entry in output:
-                output_channel = entry['output_channel']
-                rules = entry.get('rules')
-                any_matching = entry.get('any_matching')
-                hide_forward = entry.get('hide_forward')
-                all_messages = entry.get('all_messages')
-                ex_rules = entry.get('ex_rules')
+        p = Profile(doc=profile_doc)
+        p.process()
+        count += p.count
 
-                if all_messages:
-                    processAllMessages()
-                else:
-                    processRules()
-
-        profile_doc['lastupdate'] = str(datetime.now()+timedelta(hours=5))
-        db.profiles.update_one({'name' : profile_name}, {'$set': profile_doc})
-
-    actualsleep = sleeptimer - DELAY*i if sleeptimer - DELAY*i >=0 else 0
+    actualsleep = sleeptimer - DELAY*count if sleeptimer - DELAY*count > 0 else 0
     logging.info(f'Sleeping for {actualsleep} seconds...')
     time.sleep(actualsleep)
 
